f31dc8a5d1a1dca25616c583620be9fd4d40b466
Alaa Hijazi <alaa.hijayyad@gmail.com>
2022-09-01 17:50:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ languages.en = require("./en.json");
 module.exports = languages;
 module.exports.webLangs = {
   ar: { name: "العربية", flag: "sa", dir: "rtl" },
-  en: { name: "English", flag: "us", dir: "ltr },
+  en: { name: "English", flag: "us", dir: "ltr" },
 };
